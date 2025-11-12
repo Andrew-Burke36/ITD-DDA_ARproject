816e6c3260248ee96996901af827817592f49765
Andrew John Burke <s10266774@connect.np.edu.sg>
2025-11-12 13:10:51 +0800

    Unity library file
    This commit is for all the unity libraries such as assets, packages and etc.

---
+++ Assets/Scripts/FPSLock.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FPSLock : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Disable vSync so Application.targetFrameRate takes effect
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 120;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
