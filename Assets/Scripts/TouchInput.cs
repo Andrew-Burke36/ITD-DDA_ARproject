2ef12057fd62a5a55c04596265aeec1202096984
Andrew John Burke <s10266774@connect.np.edu.sg>
2025-12-02 13:54:54 +0800

    Firebase & Xcode implementation
    > Firebase implementation with database and authentication
    > Xcode works with unity to build to ios
    > implemented simple login ui and trying to link authentication to firebase

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -1,34 +1,16 @@
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
-using System.Collections.Generic;
 
 public class TouchInput : MonoBehaviour
 {
-    public GameObject objectToPlace;
-    public ARRaycastManager raycastManager;
-    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        // 1️⃣ Check if the user has touched the screen
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            // 2️⃣ Check if this is a new tap
-            if (touch.phase == TouchPhase.Began)
-            {
-                // 3️⃣ Fire a raycast from the tap position into the AR world
-                if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
-                {
-                    // 4️⃣ Get the hit position (Pose)
-                    Pose hitPose = hits[0].pose;
-
-                    // 5️⃣ Place or move your object
-                    Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
-                }
-            }
-        }
+        
     }
 }
